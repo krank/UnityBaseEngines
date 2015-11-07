a109132e84aeebae45f49d575f5006c761dc0357
Mikael Bergström <krank23@gmail.com>
2015-11-07 22:19:13 +0100

    General restructuring + cleanup. Also implemented teleports, closes #5.

---
+++ PlatformerBase/Assets/Scripts/TeleportSourceController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportSourceController : MonoBehaviour {
+
+    public GameObject teleportTarget;
+
+	// Use this for initialization
+	void Start () {
+        if (teleportTarget == null)
+        {
+            Debug.Log("Warning! Teleport source lacks target!");
+        }
+
+	}
+
+    void OnTriggerEnter2D (Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.transform.position = teleportTarget.transform.position;
+        }
+    }
+}
